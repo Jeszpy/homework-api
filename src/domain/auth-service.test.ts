98cfe7ae5dc51aeb44ff4d93db6ae397851385f0
Jezspy <hleb.lukashonak@gmail.com>
2022-06-29 17:00:56 +0300

    add tests for AuthService - createUser - prepare email to sending

--- src/domain/auth-service.test.ts
+++ src/domain/auth-service.test.ts
@@ -1,39 +1,59 @@
 import {AuthService} from "./auth-service";
-import {UsersRepository} from "../repositories/mongo-db-with-mongoose/users-repository";
-import {EmailsModel, UsersModel} from "../repositories/mongo-db-with-mongoose/models";
 import {ioc} from "../IoCContainer";
 import {TYPES} from "../types/ioc";
 import {UsersService} from "./users-service";
 import {MongoMemoryServer} from "mongodb-memory-server";
+import mongoose from "mongoose";
+import {EmailsRepository} from "../repositories/mongo-db-with-mongoose/emails-repository";
 
 describe('integration tests for AuthService', () => {
 
 
 
-    describe('createUser', () => {
+    describe('createUser && getUserInfoById && getEmailForQueue for sending ', () => {
 
         let mongoServer: MongoMemoryServer
 
         beforeAll(async () => {
             mongoServer = await MongoMemoryServer.create()
+            const mongoUri = mongoServer.getUri()
+            await mongoose.connect(mongoUri)
         })
-
-        // const m
 
         const authService = ioc.get<AuthService>(TYPES.IAuthService)
         const usersService = ioc.get<UsersService>(TYPES.IUsersService)
+        const emailRepository = ioc.get<EmailsRepository>(TYPES.IEmailsRepository)
 
         const login = 'validLogin'
         const email = 'hleb.lukashonak@yandex.ru'
         const password = 'validPassword'
 
-        it('should return', async () => {
+        it('createUser and getUserInfoById', async () => {
 
+            // create new user | returns 'id', 'login'
             const newUser = await usersService.createUser(login, email, password)
+            if (!newUser) throw Error
+            // get new user from db by ID | returns 'UserIdAndLoginType'
+            const userId = newUser.id
+            const createdUser = await usersService.getUserInfoById(userId)
+            if (!createdUser) throw Error
 
-            expect(newUser!.login).toBe(login)
+            expect(createdUser.login).toBe(login)
+            expect(createdUser.email).toBe(email)
+
         })
 
+        it('getEmailForQueue', async () => {
+
+            const emailInQueue = await emailRepository.getEmailFromQueue()
+            if (!emailInQueue) throw Error
+
+            expect(emailInQueue.email).toBe(email)
+            expect(emailInQueue.userLogin).toBe(login)
+            expect(emailInQueue.status).toBe('pending')
+            expect(emailInQueue.subject).toBe('registration')
+            expect(emailInQueue.confirmationCode).not.toBeNull()
+        })
     })
 })
 
